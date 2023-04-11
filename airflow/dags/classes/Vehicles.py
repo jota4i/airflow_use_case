f11cebc25cfa0a5e7631de7b1c9a4b52eda016ff
José (Jota) <129964889+jota4i@users.noreply.github.com>
2023-04-11 16:07:47 -0300

    feat: Add notebook executions, trigger tasks and dag documentations. (#1)
    * Add notebook execution, dag documentation and other features.
    
    * Add black lint and remove temp files.
    
    ---------
    
    Co-authored-by: Jose Adelmar <j.aldemar@4intelligence.com.br>

--- airflow/dags/classes/Vehicles.py
+++ airflow/dags/classes/Vehicles.py
@@ -1,7 +1,5 @@
 import os
 
-#LOCAL = docker/airflow/airflow
-#DOCKER = /opt/airflow/dags
 
 class GetVehicleBrand:
     def __init__(self, vehicle_type):
@@ -16,35 +14,33 @@ class GetVehicleBrand:
         self.file_path = f"{temp_path}/files/land"
         self.bucket = "airflow4i_lake"
 
-
     def get_vehicle_list(self):
         from classes.HTTPRequests import HTTPRequester
+
         request = HTTPRequester(self.url)
         response = request.make_request()
         self.brands = response.json()
 
-
     def save_to_file(self):
         import json
+
         self.file_name = f"land/brand/{self.vehicle_type}.json"
         self.full_path = f"{self.file_path}/{self.file_name}"
         print(self.file_name)
         print(self.full_path)
         os.makedirs(os.path.dirname(self.full_path), exist_ok=True)
-        with open(self.full_path, 'w') as f:
-            json.dump(self.brands, f)    
+        with open(self.full_path, "w") as f:
+            json.dump(self.brands, f)
         self.__send_to_gcp()
-
 
     def __send_to_gcp(self):
         from .Google import GCSUploader
-        upload_to_gcs = GCSUploader(self.full_path,self.file_name,self.bucket)
+
+        upload_to_gcs = GCSUploader(self.full_path, self.file_name, self.bucket)
         upload_to_gcs.upload()
 
-    
+
 class GetVehicleModel:
-
-
     def __init__(self, vehicle_type, cod_model):
         local_test = "docker"
         if local_test == "docker":
@@ -60,27 +56,28 @@ class GetVehicleModel:
 
     def get_model_detail(self):
         from classes.HTTPRequests import HTTPRequester
+
         request = HTTPRequester(self.url)
         response = request.make_request()
         models = response.json()
         self.models = models["modelos"]
 
-
     def save_to_file(self):
         import json
+
         self.file_name = f"models/{self.vehicle_type}_{self.cod_model}.json"
         self.full_path = f"{self.file_path}/{self.file_name}"
         print(self.file_name)
         print(self.full_path)
         os.makedirs(os.path.dirname(self.full_path), exist_ok=True)
-        with open(self.full_path, 'w') as f:
-            json.dump(self.models, f)    
+        with open(self.full_path, "w") as f:
+            json.dump(self.models, f)
         self.__send_to_gcp()
-
 
     def __send_to_gcp(self):
         from Google import GCSUploader
-        upload_to_gcs = GCSUploader(self.full_path,self.file_name,self.bucket)
+
+        upload_to_gcs = GCSUploader(self.full_path, self.file_name, self.bucket)
         upload_to_gcs.upload()
 
 
@@ -101,26 +98,27 @@ class GetVehicleModelYear:
 
     def get_year_detail(self):
         from classes.HTTPRequests import HTTPRequester
+
         request = HTTPRequester(self.url)
         response = request.make_request()
         self.years = response.json()
 
-
     def save_to_file(self):
         import json
+
         self.file_name = f"{self.vehicle_type}_{self.cod_model}_{self.cod_year}.json"
         self.full_path = f"{self.file_path}/{self.file_name}"
         print(self.file_name)
         print(self.full_path)
         os.makedirs(os.path.dirname(self.full_path), exist_ok=True)
-        with open(self.full_path, 'w') as f:
-            json.dump(self.years, f)    
+        with open(self.full_path, "w") as f:
+            json.dump(self.years, f)
         self.__send_to_gcp()
-
 
     def __send_to_gcp(self):
         from Google import GCSUploader
-        upload_to_gcs = GCSUploader(self.full_path,self.file_name,self.bucket)
+
+        upload_to_gcs = GCSUploader(self.full_path, self.file_name, self.bucket)
         upload_to_gcs.upload()
 
 
@@ -142,24 +140,27 @@ class GetVehicleDetails:
 
     def get_detail(self):
         from classes.HTTPRequests import HTTPRequester
+
         request = HTTPRequester(self.url)
         response = request.make_request()
         self.details = response.json()
 
-
     def save_to_file(self):
         import json
-        self.file_name = f"{self.vehicle_type}_{self.cod_model}_{self.cod_year}_{self.year}.json"
+
+        self.file_name = (
+            f"{self.vehicle_type}_{self.cod_model}_{self.cod_year}_{self.year}.json"
+        )
         self.full_path = f"{self.file_path}/{self.file_name}"
         print(self.file_name)
         print(self.full_path)
         os.makedirs(os.path.dirname(self.full_path), exist_ok=True)
-        with open(self.full_path, 'w') as f:
-            json.dump(self.details, f)    
+        with open(self.full_path, "w") as f:
+            json.dump(self.details, f)
         self.__send_to_gcp()
-
 
     def __send_to_gcp(self):
         from Google import GCSUploader
-        upload_to_gcs = GCSUploader(self.full_path,self.file_name,self.bucket)
+
+        upload_to_gcs = GCSUploader(self.full_path, self.file_name, self.bucket)
         upload_to_gcs.upload()
